d5270b1c35303b81abd7f8444d98ed8bcefa3e72
alexlin5408 <67335068+alexlin5408@users.noreply.github.com>
2025-02-04 16:05:49 -0500

    Updated integration to return token info, Updated profile page to show Button

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -1,1 +1,31 @@
 <h1>Profile</h1>
+
+<script>
+    import Integration_Button from '../../components/integration-button.svelte';
+</script>
+  
+<main>
+    <div class="card">
+      <Integration_Button company="Google" />
+    </div>
+  
+    <div class="card">
+      <Integration_Button company="Microsoft" />
+    </div>
+  
+    <div class="card">
+      <Integration_Button company="Notion" />
+    </div>
+</main>
+  
+<style>
+    main {
+      padding: 20px;
+      max-width: 600px;
+      margin: 0 auto;
+    }
+  
+    .card {
+      margin-bottom: 16px;
+    }
+</style>
