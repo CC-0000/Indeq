96047256921d3d7674a8e54b8d28c25af9f0c8ad
patrickklapid@gmail.com <patrickklapid@gmail.com>
2025-02-05 21:39:56 -0500

    feat(frontend): configured shadcn-svelte + auth ui

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,1 +1,44 @@
-<h1>Login</h1>
+<script lang="ts">
+	import * as Card from "$lib/components/ui/card/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<div class="min-h-screen flex items-center justify-center">
+	<div class="min-w-96 flex flex-col gap-4">
+		<Card.Root class="w-full max-w-sm mx-auto">
+			<Card.Header class="space-y-1">
+				<Card.Title class="text-2xl">Welcome back</Card.Title>
+				<Card.Description>Enter your email below to login to your account</Card.Description>
+			</Card.Header>
+			<Card.Content class="grid gap-4">
+				<Button variant="outline">
+					<img src="/google.svg" alt="Google logo" class="mr-2 h-6 w-6" />
+					Login with Google
+				</Button>
+				<div class="relative">
+					<div class="absolute inset-0 flex items-center">
+						<span class="w-full border-t"></span>
+					</div>
+					<div class="relative flex justify-center text-xs">
+						<span class="bg-card text-muted-foreground px-2"> Or continue with </span>
+					</div>
+				</div>
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" placeholder="m@example.com" />
+				</div>
+				<div class="grid gap-2">
+					<Label for="password">Password</Label>
+					<Input id="password" type="password" />
+				</div>
+			</Card.Content>
+			<Card.Footer class="flex flex-col gap-4">
+				<Button class="w-full">Login</Button>
+				<Card.Description class="relative text-center">Don't have an account? <a class="underline hover:text-primary" href="/register">Sign up</a></Card.Description>
+			</Card.Footer>
+		</Card.Root>
+        <Card.Description class="text-center text-sm text-muted-foreground">&nbsp;</Card.Description>
+	</div>
+</div>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,1 +1,46 @@
-<h1>Register</h1>
+<script lang="ts">
+	import * as Card from "$lib/components/ui/card/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<div class="min-h-screen flex items-center justify-center">
+	<div class="min-w-96 flex flex-col gap-4">
+		<Card.Root class="w-full max-w-sm mx-auto">
+			<Card.Header class="space-y-1">
+				<Card.Title class="text-2xl">Sign up</Card.Title>
+				<Card.Description>Enter your email below to create an account</Card.Description>
+			</Card.Header>
+			<Card.Content class="grid gap-4">
+				<Button variant="outline">
+					<img src="/google.svg" alt="Google logo" class="mr-2 h-6 w-6" />
+					Sign up with Google
+				</Button>
+				<div class="relative">
+					<div class="absolute inset-0 flex items-center">
+						<span class="w-full border-t"></span>
+					</div>
+					<div class="relative flex justify-center text-xs">
+						<span class="bg-card text-muted-foreground px-2"> Or continue with </span>
+					</div>
+				</div>
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" placeholder="m@example.com" />
+				</div>
+				<div class="grid gap-2">
+					<Label for="password">Password</Label>
+					<Input id="password" type="password" />
+				</div>
+			</Card.Content>
+			<Card.Footer class="flex flex-col gap-4">
+				<Button class="w-full">Create account</Button>
+				<Card.Description class="relative text-center">Already have an account? <a class="underline hover:text-primary" href="/login">Login</a></Card.Description>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Description class="text-center text-sm text-muted-foreground">
+			By clicking continue, you agree to our <a href="/terms" class="underline hover:text-primary">Terms of Service</a> and <a href="/privacy" class="underline hover:text-primary">Privacy Policy</a>.
+		</Card.Description>
+	</div>
+</div>
