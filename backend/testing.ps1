0c730df56399b8af43b74ef48053969675d6ee44
Xian Zhang <xianjianzhang.yahoo@gmail.com>
2025-02-24 13:19:55 -0500

    syntax + removed script file

--- backend/testing.ps1
+++
@@ -1,9 +0,0 @@
-# Get the list of services defined in the docker-compose.yml
-$services = docker-compose config --services
-
-# Loop through each service
-foreach ($service in $services) {
-    # Tag the image
-    Write-Host "$service"
-
-}
