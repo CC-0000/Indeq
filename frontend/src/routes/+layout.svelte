e4756e7bfb0cc3611d9ddaf66ee6a897bc16b357
patrickklapid@gmail.com <patrickklapid@gmail.com>
2025-03-06 02:37:17 -0500

    feat(analytics): vercel analytics

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from "$lib/components/ui/sonner";
+    import { injectAnalytics } from '@vercel/analytics/sveltekit'
 
 	let { children } = $props();
+    
+    // Vercel Analytics
+    injectAnalytics({
+        debug: false,
+        mode: 'production'
+    });
 </script>
 
 <Toaster theme="light" />
