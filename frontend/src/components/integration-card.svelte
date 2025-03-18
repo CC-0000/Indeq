92f767988e9744216d0a874996d182a7710992df
Amay Patel <92037532+amay-patel@users.noreply.github.com>
2025-03-17 20:08:15 -0400

    Update integration-card.svelte

--- frontend/src/components/integration-card.svelte
+++ frontend/src/components/integration-card.svelte
@@ -1,22 +1,3 @@
-<!-- <script>
-	export let company = 'Fill';
-	export let isIntegrated = false;
-	import { Button } from '$lib/components/ui/button/index.js';
-	function authenticate() {
-		window.location.href = `/profile/integration?provider=${company.toUpperCase()}`;
-	}
-</script>
-
-<Button variant="outline" on:click={authenticate}>
-	<span class="company-name">
-		{#if isIntegrated}
-			Disconnect
-		{:else}
-			Connect
-		{/if}
-	</span>
-</Button> -->
-
 <script>
 	import { Button } from '$lib/components/ui/button/index.js';
 	export let company = 'Fill';
